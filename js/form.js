b7c994089a911556629b3ada4598782cdcac4753
Ilya Emelyanov <baralgin38@yandex.ru>
2021-06-21 15:49:40 +0300

    Добавляет функции изменяющие активность форм
    Функции принимают в качестве параметров форму на которой будет добавлен
    деактивирующий класс и внутри которой будет происходить поиск элементов
    для деактивации, также принимает булевый флаг в зависимости от которого
    форма с её дочерними элементами будет активироваться/деактивироваться.

--- js/form.js
+++ js/form.js
@@ -1,16 +1,46 @@
 const adForm = document.querySelector('.ad-form');
+const mapFiltersForm = document.querySelector('.map__filters');
 
-const formFieldsetDeactivating = (form, isDeactivated) => {
-  const formFieldset = form.querySelectorAll('fieldset');
+const formElementsDeactivating = (form, element, isDeactivated) => {
+  const formFieldset = form.querySelectorAll(element);
   for(let i = 0; i < formFieldset.length; i++) {
     formFieldset[i].disabled = isDeactivated;
   }
 };
 
-const adFormDeactivating = (form) => {
-  form.classList.add('ad-form--disabled');
+// const adFormDeactivating = (form) => {
+//   form.classList.add('ad-form--disabled');
+//   formElementsDeactivating(form, 'fieldset', true);
+// };
 
-  formFieldsetDeactivating(form, true);
+// const mapFiltersFormDeactivating = (form) => {
+//   form.classList.add('map__filters--disabled');
+//   formElementsDeactivating(form, 'select', true);
+//   formElementsDeactivating(form, 'fieldset', true);
+// };
+
+// adFormDeactivating(adForm);
+// mapFiltersFormDeactivating(mapFiltersForm);
+
+
+const changeActivityAdForm = (form, isDeactivated) => {
+  if (isDeactivated) {
+    form.classList.add('ad-form--disabled');
+  } else {
+    form.classList.remove('ad-form--disabled');
+  }
+  formElementsDeactivating(form, 'fieldset', isDeactivated);
 };
 
-adFormDeactivating(adForm);
+const changeActivityMapFiltersForm = (form, isDeactivated) => {
+  if (isDeactivated) {
+    form.classList.add('map__filters--disabled');
+  } else {
+    form.classList.remove('map__filters--disabled');
+  }
+  formElementsDeactivating(form, 'select', isDeactivated);
+  formElementsDeactivating(form, 'fieldset', isDeactivated);
+};
+
+changeActivityAdForm(adForm, false);
+changeActivityMapFiltersForm(mapFiltersForm, false);
