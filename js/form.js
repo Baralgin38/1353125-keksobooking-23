ed7ae939a95352170f58aba0a325fb26ef2aa1d8
Ilya Emelyanov <baralgin38@yandex.ru>
2021-06-28 15:11:39 +0300

    Добавляет валидацию для поля с ценой за ночь
    - созданы 2 функции setValidationOnPriceInput и
      setDependencyBetweenHousingTypeAndPriceInput отвечающие за валидацию
      поля с ценой.
    - setDependencyBetweenHousingTypeAndPriceInput в зависимости от
       значения типа жилья, добавляет минимальную границу для цены за ночь
       и добавляет плэйсхолдер с этим значением.
    - setValidationOnPriceInput добавляет возможность при каждом изменении
       поля с ценой, выводить подсказку если значение не корректно.

--- js/form.js
+++ js/form.js
@@ -1,6 +1,5 @@
 const TITLE_MAX_LENGTH = 100;
 const TITLE_MIN_LENGTH = 30;
-const PRICE_MAX_VALUE = 1000000;
 
 const formElementsDeactivating = (form, element, isDeactivated) => {
   const formFieldset = form.querySelectorAll(element);
@@ -8,7 +7,6 @@ const formElementsDeactivating = (form, element, isDeactivated) => {
     formFieldset[i].disabled = isDeactivated;
   }
 };
-
 
 const changeActivityAdForm = (form, isDeactivated) => {
   if (isDeactivated) {
@@ -29,20 +27,66 @@ const changeActivityMapFiltersForm = (form, isDeactivated) => {
   formElementsDeactivating(form, 'fieldset', isDeactivated);
 };
 
-const setValidationOnTitleInput = (titleInput) => {
-  titleInput.addEventListener('input', () => {
-    const valueLength = titleInput.value.length;
+const setValidationOnTitleInput = (input) => {
+  input.addEventListener('input', () => {
+    const valueLength = input.value.length;
 
     if (valueLength < TITLE_MIN_LENGTH) {
-      titleInput.setCustomValidity(`Нужно ввести ещё ${TITLE_MIN_LENGTH - valueLength} симв.`);
+      input.setCustomValidity(`Нужно ввести ещё ${TITLE_MIN_LENGTH - valueLength} симв.`);
     } else if (valueLength > TITLE_MAX_LENGTH) {
-      titleInput.setCustomValidity(`Удалите лишние ${valueLength - TITLE_MAX_LENGTH} симв.`);
+      input.setCustomValidity(`Удалите лишние ${valueLength - TITLE_MAX_LENGTH} симв.`);
     } else {
-      titleInput.setCustomValidity('');
+      input.setCustomValidity('');
     }
 
-    titleInput.reportValidity();
+    input.reportValidity();
   });
 };
 
-export {changeActivityAdForm, changeActivityMapFiltersForm, setValidationOnTitleInput};
+const setValidationOnPriceInput = (input) => {
+  input.addEventListener('input', () => {
+
+    input.reportValidity();
+  });
+};
+
+const setPriceInputAttribute = (type, input) => {
+  switch(type.value) {
+    case 'bungalow':
+      input.min = 0;
+      input.placeholder = 0;
+      break;
+    case 'flat':
+      input.min = 1000;
+      input.placeholder = 1000;
+      break;
+    case 'hotel':
+      input.min = 3000;
+      input.placeholder = 3000;
+      break;
+    case 'house':
+      input.min = 5000;
+      input.placeholder = 5000;
+      break;
+    case 'palace':
+      input.min = 10000;
+      input.placeholder = 10000;
+      break;
+  }
+};
+
+const setDependencyBetweenHousingTypeAndPriceInput = (type, input) => {
+  setPriceInputAttribute(type, input);
+
+  type.addEventListener('change', () => {
+    setPriceInputAttribute(type, input);
+  });
+};
+
+export {
+  changeActivityAdForm,
+  changeActivityMapFiltersForm,
+  setValidationOnTitleInput,
+  setValidationOnPriceInput,
+  setDependencyBetweenHousingTypeAndPriceInput
+};
