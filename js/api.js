c7a0e1f80dda20653a11f5a0893e2d48eee5fb1c
Ilya Emelyanov <baralgin38@yandex.ru>
2021-07-08 22:15:36 +0300

    Добавляет обработку ошибок при загрузке данных

--- js/api.js
+++ js/api.js
@@ -1,8 +1,19 @@
+import {showAlert} from './utils/util.js';
+
 const getData = (onSuccess) => {
   fetch('https://23.javascript.pages.academy/keksobooking/data')
-    .then((response) => response.json())
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+
+      throw new Error(`${response.status} ${response.statusText}`);
+    })
     .then((data) => {
       onSuccess(data);
+    })
+    .catch((err) => {
+      showAlert(err);
     });
 
 };
