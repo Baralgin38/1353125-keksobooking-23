55ff196cdc795578b86f0a081c73a5028d994b07
Ilya Emelyanov <baralgin38@yandex.ru>
2021-07-14 16:36:25 +0300

    Убирает опечатку в url для fetch

--- js/api.js
+++ js/api.js
@@ -20,7 +20,7 @@ const getData = (onSuccess) => {
 
 const sendData = (onSuccess, onFail, body) => {
   fetch(
-    'https://23.javascript.pages.academy/keksobookingg',
+    'https://23.javascript.pages.academy/keksobooking',
     {
       method: 'POST',
       body,
