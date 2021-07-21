128b0756305226921d64b91c1642692d34bc731c
Ilya Emelyanov <baralgin38@yandex.ru>
2021-07-21 14:19:32 +0300

    Меняет логику сброса форм
    - При нажатии на кнопку "очистить" сбрасываются данные форм и удаляются
      метки находящиеся на карте (отфильтрованные предыдущими фильтрами).
      Затем отрисовываются 10 случайных меток

--- js/reset-form.js
+++ js/reset-form.js
@@ -1,5 +1,6 @@
 import {setAddressValue, setMainPinDefaultCoordinates} from './map.js';
 import {setPriceInputAttribute, setAllowedQuantityGuests} from './form.js';
+import {clearMarkerGroup} from './map.js';
 
 const adForm = document.querySelector('.ad-form');
 const mapFiltersForm = document.querySelector('.map__filters');
@@ -15,9 +16,18 @@ const resetForms = () => {
 
 const adFormResetButton = adForm.querySelector('.ad-form__reset');
 
-adFormResetButton.addEventListener('click', (evt) => {
-  evt.preventDefault();
-  resetForms();
-});
 
-export {resetForms};
+const setResetOnForms = (cb) => {
+  adFormResetButton.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    resetForms();
+    clearMarkerGroup();
+    cb();
+  });
+};
+// adFormResetButton.addEventListener('click', (evt) => {
+//   evt.preventDefault();
+//   resetForms();
+// });
+
+export {resetForms, setResetOnForms};
